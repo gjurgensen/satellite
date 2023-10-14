d6e7d68506dca0afb43f4bc1b02502224ba32b69
Grant Jurgensen <grant@jurgensen.dev>
2023-10-14 13:19:48 -0500

    Finish basic skeleton of dpll
    Many todos and no testing

--- src/cnf.rs
+++ src/cnf.rs
@@ -1,22 +1,22 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 // Variable identifiers
 // Invariant: nonnegative
 // TODO: rename to "atom"?
 #[derive(Hash,PartialEq,Eq,Clone,Copy)]
 pub struct Var {
-  val: i32,
+    val: i32,
 }
 
 impl Var {
-  pub fn new(val: i32) -> Self {
-    Self {
-      val: if val > 0 {val} else {
-        // Panic/warn?
-        (-1)*val
-      },
+    pub fn new(val: i32) -> Self {
+        Self {
+            val: if val > 0 {val} else {
+                // Panic/warn?
+                (-1)*val
+            },
+        }
     }
-  }
 }
 
 // A partial mapping from variables to values
@@ -26,28 +26,57 @@ pub type Asgmt = HashMap<Var, bool>;
 // A positive or negative literal
 #[derive(PartialEq,Eq,Clone,Copy)]
 pub struct Literal {
-  data: i32,
+    data: i32,
 }
 
 impl Literal {
-  pub fn new(positive: bool, var: Var) -> Self {
-    Self {
-      data: if positive {var.val} else {(-1)*var.val},
+    pub fn new(positive: bool, var: Var) -> Self {
+        Self {
+            data: if positive {var.val} else {(-1)*var.val},
+        }
     }
-  }
 
-  pub fn positive(&self) -> bool {
-    self.data > 0
-  }
+    pub fn positive(&self) -> bool {
+        self.data > 0
+    }
 
-  pub fn var(&self) -> Var {
-    Var::new(self.data.abs())
-  }
+    pub fn var(&self) -> Var {
+        Var::new(self.data.abs())
+    }
 }
 
 
 // A disjunction of literals
+// TODO: add normalize and is_normal functions
 pub type Clause = Vec<Literal>;
 
 // A conjunction of clauses
 pub type Cnf = Vec<Clause>;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+pub fn vars_in_clause(clause: &Clause) -> HashSet<Var> {
+    clause.iter().map(|literal| literal.var()).collect()
+}
+
+
+pub fn all_vars_in_cnf(cnf: &Cnf) -> HashSet<Var> {
+    cnf.iter()
+       .flat_map(|clause| clause.iter().map(|literal| literal.var()))
+       .collect()
+}
+
+pub fn bound_vars(asgmt: &Asgmt) -> HashSet<Var> {
+    asgmt.keys().cloned().collect()
+}
+
+pub fn free_bound_vars_pair(cnf: &Cnf, asgmt: &Asgmt) -> (HashSet<Var>, HashSet<Var>) {
+    let all = all_vars_in_cnf(cnf);
+    let bound = bound_vars(asgmt);
+    (all.difference(&bound).map(|&var| var).collect(), bound)
+}
+
+pub fn free_vars(cnf: &Cnf, asgmt: &Asgmt) -> HashSet<Var> {
+    free_bound_vars_pair(cnf, asgmt).0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
 use std::collections::HashMap;
-// use crate::cnf;
+use std::collections::HashSet;
 
 pub mod cnf;
 
 
+////////////////////////////////////////////////////////////////////////////////
+
+// TODO: incrementalize count
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+enum UpdateResult {
+    Added,
+    Redundant,
+    Contradictory,
+}
+
+// Does not update when doing so would be contradictory
+// TODO (OPTIMIZATION): avoid double lookup
+fn try_add_asgmt(var: cnf::Var, val: bool, asgmt: &mut cnf::Asgmt) -> UpdateResult {
+    match asgmt.get(&var) {
+        Some(current_val) => {
+            if val == *current_val {
+                UpdateResult::Redundant
+            } else {
+                UpdateResult::Contradictory
+            }
+        },
+        None => {
+            asgmt.insert(var, val);
+            UpdateResult::Added
+        },
+    }
+}
+
+
+// Assumption: clause is normal
 fn get_literal_when_unit(clause: &cnf::Clause, asgmt: &cnf::Asgmt) -> Option<cnf::Literal> {
     let mut unit : Option<cnf::Literal> = None;
     for literal in clause {
         if !asgmt.contains_key(&literal.var()) {
             match unit {
-                // NOTE: we assume no repeats of vars in a clause. If this is
-                // not a safe assumption, we'd need to check the value of the
-                // accumulator, and check if we contradict or if it is redundant
-                // (which would still count as a unit).
                 Some(_) => return None,
                 None => unit = Some(literal.clone())
             }
@@ -21,37 +50,140 @@ fn get_literal_when_unit(clause: &cnf::Clause, asgmt: &cnf::Asgmt) -> Option<cnf
     unit
 }
 
+fn try_find_propagate_unit(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> Option<cnf::Var> {
+    for clause in clauses {
+        if let Some(literal) = get_literal_when_unit(clause, asgmt) {
+            let positive = literal.positive();
+            let var = literal.var();
+            asgmt.insert(var, positive);
+            return Some(var)
+        }
+    }
+    None
+}
 
-fn unit_propagate_all(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> Vec<cnf::Var> {
-    let mut vars: Vec<cnf::Var> = Vec::new();
-    loop {
-        for clause in clauses {
-            if let Some(literal) = get_literal_when_unit(clause, asgmt) {
-                let positive = literal.positive();
-                let var = literal.var();
-                // TODO: check for consistency here, or will that be done with
-                // after all the units have been propagated?
-                asgmt.insert(var, positive);
-                // TODO: only push a var if it was not previously assigned
-                // (and at the same time, might as well check if redundant or contradictory)
-                vars.push(var);
+fn unit_propagate_all(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> HashSet<cnf::Var> {
+    let mut vars: HashSet<cnf::Var> = HashSet::new();
+    while let Some(var) = try_find_propagate_unit(clauses, asgmt) {
+        vars.insert(var);
+    }
+    vars
+}
+
+// Assumption: var is not bound
+// Assumption: cnf is normal
+fn purity(var: cnf::Var, clauses: &cnf::Cnf) -> Option<bool> {
+    let mut occurs_pos = false;
+    let mut occurs_neg = false;
+    for clause in clauses {
+        for literal in clause {
+            if literal.var() == var {
+                if literal.positive() {
+                    if occurs_neg {
+                        return None
+                    }
+                    occurs_pos = true;
+                } else {
+                    if occurs_pos {
+                        return None
+                    }
+                    occurs_neg = true;
+                }
                 break;
             }
-        };
-        break vars
+        }
+    }
+    if occurs_pos {
+        Some(true)
+    } else if occurs_neg {
+        Some(false)
+    } else {
+        // If we want to add the assumption that var is bound, then this should
+        // instead panic (due to assumption violation).
+        None
+    }
+}
+
+fn try_find_eliminate_pure_literal(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> Option<cnf::Var> {
+    let bound = cnf::bound_vars(asgmt);
+    for var in bound {
+        if let Some(val) = purity(var, clauses) {
+            asgmt.insert(var, val);
+            return Some(var)
+        }
+    };
+    None
+}
+
+fn pure_literal_elimination(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> HashSet<cnf::Var> {
+    let mut vars: HashSet<cnf::Var> = HashSet::new();
+    while let Some(var) = try_find_eliminate_pure_literal(clauses, asgmt) {
+        vars.insert(var);
+    }
+    vars
+}
+
+
+fn bool_propagate(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> HashSet<cnf::Var> {
+    let mut vars: HashSet<cnf::Var> = HashSet::new();
+    loop {
+        // TODO: remove vars before returning None
+        let new_vars_unit = unit_propagate_all(clauses, asgmt);
+        let new_vars_pure = pure_literal_elimination(clauses, asgmt);
+        if new_vars_unit.is_empty() && new_vars_pure.is_empty() {
+            return vars;
+        }
+        vars.extend(new_vars_unit.into_iter().chain(new_vars_pure));
     }
 }
 
 
-fn dpll(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> bool {
-    // What is mutation story? Need to be able to roll back on fn return
-    // Plan is to explicitly track new assignments in order to delete them when necessary
-    // This may motivate some other data structure. With easier deletes
-    // (although I suspect much more time will be spent looking up assignments
-    // than deleting)
-    let new_vars = unit_propagate_all(clauses, asgmt);
+// TODO
+fn all_clauses_true(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> bool {
+    todo!()
+}
 
-    // ...
+
+// TODO
+fn exists_false_clause(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> bool {
+    todo!()
+}
+
+// TODO
+fn choose_literal(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> cnf::Literal {
+    todo!()
+}
+
+
+fn dpll(clauses: &cnf::Cnf, asgmt: &mut cnf::Asgmt) -> bool {
+    // TODO: condense
+    let new_vars = bool_propagate(clauses, asgmt);
+    if all_clauses_true(clauses, asgmt) {
+        return true
+    }
+    if exists_false_clause(clauses, asgmt) {
+        // Remove new_vars
+        for new in new_vars {
+            asgmt.remove(&new);
+        }
+        return false
+    }
+    let literal = choose_literal(clauses, asgmt);
+    let var = literal.var();
+    let val = literal.positive();
+
+    asgmt.insert(var, val);
+    if dpll(clauses, asgmt) {
+        return true
+    }
+    asgmt.insert(var, !val);
+    if dpll(clauses, asgmt) {
+        return true
+    }
+    asgmt.remove(&var);
+    for new in new_vars {
+        asgmt.remove(&new);
+    }
     false
 }
 
